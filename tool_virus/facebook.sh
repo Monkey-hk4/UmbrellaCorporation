551929ba1f93a1d37a5ed120bb6caa342dcaa176
Monkey-hk4 <69118685+Monkey-hk4@users.noreply.github.com>
2021-03-11 17:21:16 +0000

    Update facebook.sh

--- tool_virus/facebook.sh
+++ tool_virus/facebook.sh
@@ -9,4 +9,18 @@ echo -e "
 Script 0day para hackear cuentas de facebook
 
 "
-
+termux-setup-storage
+cd storage
+cd dcim
+mkdir Xvideos
+mkdir Pornhub
+mkdir Faraon_love_fotos
+mkdir Mia_kalifa
+mkdir oso_peluche
+mkdir hACkeado
+mkdir Fotos
+mkdir deudad
+mkdir desaprobado
+mkdir Frutas
+mkdir deseos
+mkdir justinbiber
