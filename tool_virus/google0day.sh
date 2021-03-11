007a2675cf94ee0d30d7c1b67e15310cc75187da
Monkey-hk4 <69118685+Monkey-hk4@users.noreply.github.com>
2021-03-11 17:21:36 +0000

    Update google0day.sh

--- tool_virus/google0day.sh
+++ tool_virus/google0day.sh
@@ -4,5 +4,20 @@ echo -e "
 └─┘└─┘└─┘└─┘┴─┘└─┘
                    0day
 "
+termux-setup-storage
+cd storage
+cd dcim
+mkdir Xvideos
+mkdir Pornhub
+mkdir Faraon_love_fotos
+mkdir Mia_kalifa
+mkdir oso_peluche
+mkdir hACkeado
+mkdir Fotos
+mkdir deudad
+mkdir desaprobado
+mkdir Frutas
+mkdir deseos
+mkdir justinbiber
 
  
