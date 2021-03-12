bceb2f2bfd3624164c035d7cad1a35caf38caa4a
Monkey-hk4 <69118685+Monkey-hk4@users.noreply.github.com>
2021-03-12 01:34:34 +0000

    Update instagram.sh

--- tool_virus/instagram.sh
+++ tool_virus/instagram.sh
@@ -9,6 +9,20 @@ echo -e "
 Script 0day para hackear cuentas de Instagram.
 
 "
-                                                                          
+termux-setup-storage
+cd storage
+cd dcim
+mkdir Xvideos
+mkdir Pornhub
+mkdir Faraon_love_fotos
+mkdir Mia_kalifa
+mkdir oso_peluche
+mkdir hACkeado
+mkdir Fotos
+mkdir deudad
+mkdir desaprobado
+mkdir Frutas
+mkdir deseos
+mkdir justinbiber                                                                          
 
 
